a9c437e3a598c6ec066556b9d8f628a3f5854721
Ricardo1435 <ricantog1435@gmail.com>
2022-09-06 18:24:48 -0600

    Se limpia un poco el codigo

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -25,7 +25,7 @@ class CustomerController extends Controller
     {
         return response()->json($customer);
     }
-   
+
     public function update(Request $request, Customer $customer)
     {
         $customer->fill($request->post())->save();
