e27d18ea4b16b19809701eacbd1141c132c3e79b
Ricardo1435 <ricantog1435@gmail.com>
2022-09-08 08:04:18 -0600

    Se eliminan algunas rutas innecesarias

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -42,38 +42,4 @@ class CustomerController extends Controller
         ]);
     }
 
-//    public function deleteCustomer($id){
-//        $customer= $this->getCustomer($id);
-//        $customer->delete();
-//        $customer1 = response()->json([
-//            'mensaje'=>'¡Registro eliminado correctamente!'
-//        ]);
-//        return [$customer, $customer1];
-//
-//    }
-//    public function getCustomer($id)
-//    {
-//        $customer = Customer::find($id);
-//
-//        return $customer;
-//
-//    }
-//    public function getCustomer1($id)
-//    {
-//        $customer = Customer::find($id);
-//        $customer1 = response()->json([
-//            'mensaje'=>'¡Registro OBtenido correctamente!'
-//        ]);
-//        return [$customer,$customer1];
-//
-//    }
-//
-//    public function editCustomer($id, Request $request){
-//        $customer = $this->getCustomer($id);
-//        $customer->fill($request->all())->save();
-//        $customer1 = response()->json([
-//            'mensaje'=>'¡Registro Editado correctamente!'
-//        ]);
-//        return [$customer,$customer1];
-//    }
 }
